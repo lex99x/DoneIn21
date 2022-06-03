c2f54c6bb1b3d1132dc8f97b5343710659d13521
gabrieldigon <gabrieldiasgonca@gmail.com>
2022-06-03 01:38:32 -0400

    adicionada opacidade dos cards

--- experimentingwithUIkit/experimentingwithUIkit/ViewController2.swift
+++ experimentingwithUIkit/experimentingwithUIkit/ViewController2.swift
@@ -4,12 +4,14 @@ import UIKit
 
 class ExerciseDetail: UIViewController {
 
+    @IBOutlet weak var scrollwhatIs: UIScrollView!
+    @IBOutlet weak var scrollBenefits: UIScrollView!
+    
     @IBOutlet weak var IdidButton: UIButton!
     
+    @IBOutlet weak var detailImage: UIImageView!
     @IBOutlet weak var exercisename: UILabel!
-    
     @IBOutlet weak var whatIs: UITextView!
-    
     @IBOutlet weak var benefits: UITextView!
     
     var clickedCard:CardDetail = CardDetail()
@@ -20,6 +22,9 @@ class ExerciseDetail: UIViewController {
         exercisename.text = clickedCard.exercisename
         whatIs.text = clickedCard.whatIs
         benefits.text = clickedCard.benefits
+        detailImage.image = clickedCard.detailImage
+        scrollwhatIs.layer.opacity = 0.9
+        scrollBenefits.layer.opacity = 0.9
     }
 
   
--- experimentingwithUIkit/experimentingwithUIkit/cardDetail.swift
+++ experimentingwithUIkit/experimentingwithUIkit/cardDetail.swift
@@ -9,14 +9,16 @@ class CardDetail
     var cardname : String
     var backgroundColor : UIColor
     var cardImage : UIImage
+    var detailImage : UIImage
     
-    init (exercisename:String,whatIs:String,benefits:String,cardname:String,background:UIColor,cardImage:UIImage){
+    init (exercisename:String,whatIs:String,benefits:String,cardname:String,background:UIColor,cardImage:UIImage,detailImage:UIImage){
         self.exercisename = exercisename
         self.whatIs = whatIs
         self.benefits = benefits
         self.cardname = cardname
         self.backgroundColor = background
         self.cardImage = cardImage
+        self.detailImage = detailImage
     }
     init(){
         self.exercisename = ""
@@ -25,6 +27,7 @@ class CardDetail
         self.cardname = ""
         self.backgroundColor = UIColor.systemGray5
         self.cardImage = UIImage(systemName: "figure.walk")!
+        self.detailImage = UIImage(systemName: "figure.walk")!
     }
 
 }

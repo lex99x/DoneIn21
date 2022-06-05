609a05448b4280b0d0a3fe1111d932904832ccbb
Alex A. Rocha <lex99r@gmail.com>
2022-06-05 15:37:42 -0400

    adicionando diretórios mvc

---
+++ experimentingwithUIkit/experimentingwithUIkit/Controllers/IntroViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewController.swift
+//  experimentingwithUIkit
+//
+//  Created by Alex A. Rocha on 05/06/22.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ experimentingwithUIkit/experimentingwithUIkit/Models/User.swift
@@ -0,0 +1,34 @@
+//
+//  User.swift
+//  experimentingwithUIkit
+//
+//  Created by Alex A. Rocha on 05/06/22.
+//
+
+class User {
+    
+    private var name: String
+    private var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    public func setName(name: String) {
+        self.name = name
+    }
+    
+    public func getName() -> String {
+        return self.name
+    }
+    
+    public func setAge(age: Int) {
+        self.age = age
+    }
+    
+    public func getAge() -> Int {
+        return self.age
+    }
+    
+}
